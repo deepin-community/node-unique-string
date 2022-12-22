58cf3cfc0b986a258293e63235b24729376783e8
Deepin Developer <deepin-dev@deepin.org>
2022-12-22 14:08:45 +0800

    feat: Init commit
    init node-unique-string
    
    Log:

---
+++ test.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+import uniqueString from '.';
+
+test('main', t => {
+	t.is(uniqueString().length, 32);
+
+	const created = new Set();
+
+	for (let i = 0; i < 100000; i++) {
+		const string = uniqueString();
+
+		if (created.has(string)) {
+			t.fail(`${string} already exists`);
+		}
+
+		t.is(string.length, 32);
+
+		created.add(string);
+	}
+});
